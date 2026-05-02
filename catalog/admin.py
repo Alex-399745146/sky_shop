f7b964f72019a283d9730f8490d4bb9a3bc773be
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-05-03 00:21:57 +0300

    +superuser+model_admin_panel task_4 over

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,24 @@
+"""
+Административная панель для приложения catalog.
+Регистрация моделей Category и Product с настройками отображения.
+"""
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+
+#  Регистрационная модель для Category.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка категорий."""
+
+    list_display = ("id", "name")                       #  Отображение в списке.
+
+
+#  Регистрационная модель для Product.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админка продуктов."""
+
+    list_display = ("id", "name", "price", "category")  #  Отображение в списке.
+    list_filter = ("category",)                         #  Фильтр продукции по категории.
+    search_fields = ("name", "details")                 #  Поиск по имени и описанию.
