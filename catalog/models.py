45efaa653e2cc45c3603f3782b55a9a5e09e4931
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-05-02 21:46:34 +0300

    +models+makemigrat+migrate tasks_2-3 over

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+    )
+    description = models.TextField(
+        default="Описание отсутствует",
+        verbose_name="Описание",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Наименование",
+    )
+    details = models.TextField(
+        default="Описание отсутствует",
+        verbose_name="Описание",
+    )
+    img = models.ImageField(
+        upload_to="products/",
+        blank=True,
+        null=True,
+        verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Категория",
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата последнего изменения"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["-created_at"]   # Минус = сортировка от новых к старым.
--- sky_shop/settings.py
+++ sky_shop/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 
 # Загружаем переменные окружения из файла .env
-load_dotenv()
+load_dotenv(override=True, encoding='utf-8')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
